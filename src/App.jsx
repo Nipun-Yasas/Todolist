74615059a00e32a8a84b029a90c89ac8a310f6b1
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2024-12-17 23:12:49 +0530

    implemented editing tasks and persist tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from "react"
-import Todoinput from "./components/Todoinput"
-import Todolist from "./components/Todolist"
+import React, { useState,useEffect } from "react"
+import Todoinput from "./components/TodoInput"
+import Todolist from "./components/TodoList"
+import { use } from "react"
 
 
 function App() {
 
   const [todos, setTodos] = useState([])
+  const [todoValue,setTodoValue] = useState('')
+
+  function persistData(newList){
+    localStorage.setItem('todos',JSON.stringify({todos:newList}))
+  }
 
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -16,13 +23,38 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return index !== todoIndex
     })
+    persistData(newTodoList)
+    
     setTodos(newTodoList)
   }
 
+  function handleEditTodo(index){
+    const valueToBeEdited = todos [index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+  }
+
+  useEffect(()=>{
+
+    if(!localStorage){
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if(!localTodos){
+      return
+    }
+
+    console.log(localTodos)
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+
+  },[])
+
   return (
     <>
-      <Todoinput handleAddTodos={handleAddTodos}/>
-      <Todolist handleDeleteTodo={handleDeleteTodo} todos={todos}/>
+      <Todoinput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos}/>
+      <Todolist handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos}/>
     </>
   )
 }
